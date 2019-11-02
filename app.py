c46a66e47033a643d2a5ff46c23ff994e4b77475
mariourban83 <mario.urban.83@gmail.com>
2019-11-02 07:19:49 +0000

    Initial commit with simple flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template, url_for,flash, redirect, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = 'mongodb+srv://mario_1:PEgPBNn89YWJ4GYQ@testing1-kwpyu.mongodb.net/zoo?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+@app.route('/home')
+def home(): 
+    return render_template('index.html',title='Home')
+
+if __name__ == "__main__":
+    app.run(debug=True)    
