52bff74c614503fd706b80e46705f86a976c8770
mariourban83 <mario.urban.83@gmail.com>
2019-11-02 08:22:55 +0000

    Add Procfile and requirements files

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = 'mongodb+srv://mario_1:PEgPBNn89YWJ4GYQ@testing1-kwpyu.mongodb.net/zoo?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
+@app.route('/')
 @app.route('/home')
 def home(): 
     return render_template('index.html',title='Home')
