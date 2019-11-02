35502d6627729ca86e0b9f3f943237e0bfe4e531
mariourban83 <mario.urban.83@gmail.com>
2019-11-02 20:23:34 +0000

    Create Login and Register forms, routes, pages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, url_for,flash, redirect, request
 from flask_pymongo import PyMongo
-from forms import AnimalForm
+from forms import AnimalForm, LoginForm, RegistrationForm
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
@@ -9,7 +9,6 @@ app.config['SECRET_KEY'] = 'bd57fa89f3141ef0b5546c8967a93507'
 app.config["MONGO_URI"] = 'mongodb+srv://mario_1:PEgPBNn89YWJ4GYQ@testing1-kwpyu.mongodb.net/zoo?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
-@app.route('/')
 @app.route('/home')
 def home(): 
     return render_template('index.html',title='Home')
@@ -29,6 +28,26 @@ def new_animal():
         else:
             return redirect(url_for('new_animal'))    
     return render_template('new_animal.html',title='Add New Animal', form=form )
+
+@app.route("/",methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@zoo.com' and form.password.data == 'password':
+            flash(f'Login Successful for {form.email.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+             flash('Login Unsuccessful! Please check username and password', 'danger')  
+    return render_template('login.html',title='Login', form=form )    
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register', form=form )
 
 if __name__ == "__main__":
     app.run(debug=True)    
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email,EqualTo
 
 # Create Flask Form for Adding New Animal to DB
 class AnimalForm(FlaskForm):
@@ -17,3 +17,23 @@ class AnimalForm(FlaskForm):
     source = TextAreaField('Source :', validators=[DataRequired()])
     section = TextAreaField('Section :', validators=[DataRequired()])
     other_optional = TextAreaField('Other-Optional :')
+
+# Create Flask Login Form 
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                            validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')                    
+    submit = SubmitField('Login')
+
+# Create Flask Registration Form 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=5, max=20)])
+    email = StringField('Email', 
+                            validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                    validators=[DataRequired(), EqualTo('password')])                        
+    submit = SubmitField('Sign Up')
+
