d3b792242d453e5537f5010b6da46c376e5332e2
mariourban83 <mario.urban.83@gmail.com>
2019-11-02 18:45:09 +0000

    Add route & data from MongoDB to the  animals page.

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ mongo = PyMongo(app)
 def home(): 
     return render_template('index.html',title='Home')
 
+@app.route('/animals')
+def animals():
+    animals = mongo.db.animals.find() 
+    return render_template('animals.html',title='Animals', animals=animals) 
+
 if __name__ == "__main__":
     app.run(debug=True)    
