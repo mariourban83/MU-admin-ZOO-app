41f0008451d8d938da301ab48be8981f871193d2
mariourban83 <mario.urban.83@gmail.com>
2019-11-04 07:49:47 +0000

    Add route for removing animals from mongoDb

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
+# Modules and libraries imported
 import os
 from flask import Flask, render_template, url_for,flash, redirect, request
 from flask_pymongo import PyMongo
 from forms import AddAnimalForm, LoginForm, RegistrationForm
 from bson.objectid import ObjectId
 
+# Config files for the app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bd57fa89f3141ef0b5546c8967a93507'
 app.config["MONGO_URI"] = 'mongodb+srv://mario_1:PEgPBNn89YWJ4GYQ@testing1-kwpyu.mongodb.net/zoo?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
+# Route for Home view
 @app.route('/home')
 def home(): 
     return render_template('index.html',title='Home')
 
+#  Route for Animals view with all animals pulled from mongoDB and displayed
 @app.route('/animals')
 def animals():
     animals = mongo.db.animals.find() 
     return render_template('animals.html',title='Animals', animals=animals) 
 
+#  Route for new animal view- for adding new animal to the mongoDB   
 @app.route('/new',methods=['GET','POST'])
 def new_animal():
 
@@ -28,13 +33,15 @@ def new_animal():
    
     return render_template('new_animal.html',title='Add New Animal', animal={})
 
-
+#  Route for editing existing animal
+#  After selecting edit-animal on page, data are transfered and populated 
+#  in the form ready for editing
 @app.route('/edit_animal/<animal_id>')
 def edit_animal(animal_id):
     animal =  mongo.db.animals.find_one({"_id": ObjectId(animal_id)})
     return render_template('edit_animal.html', animal=animal,title='Edit Animal')
 
-
+# Function called after submitting for updating animal in mongoDB
 @app.route('/update_animal/<animal_id>', methods=['GET','POST'])
 def update_animal(animal_id):
     animals = mongo.db.animals
@@ -55,6 +62,14 @@ def update_animal(animal_id):
     })
     return redirect(url_for('animals'))
 
+#  Route for removing animal from the database
+@app.route('/delete_animal/<animal_id>')
+def delete_animal(animal_id):
+    mongo.db.animals.remove({'_id': ObjectId(animal_id)})
+    return redirect(url_for('animals'))
+
+#  Default route , sets Login as landing view page
+#  Form from forms.py used for both login and register page
 @app.route("/",methods=['GET','POST'])
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -78,4 +93,4 @@ def register():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)  
+)  
