97254e30006fd953542b52f39ba9c46cea74bc30
mariourban83 <mario.urban.83@gmail.com>
2019-11-06 00:23:37 +0000

    Add config files for Heroku

--- app.py
+++ app.py
@@ -93,4 +93,6 @@ def register():
     return render_template('register.html',title='Register', form=form )
 
 if __name__ == '__main__':
-    app.run(debug = True) 
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
